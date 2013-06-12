295e9046c18eebab1ccdfccb3282f39efad9089c
Joey Scarr <joey.scarr+github@gmail.com>
2013-06-12 15:35:37 +1200

    Fixed a bug in the batch script.

--- start_openslat.bat
+++ start_openslat.bat
@@ -6,8 +6,8 @@ start node app.js & echo Starting Node server...
 REM See if the server is listening on port 8080
 set count=0
 :begin
-netstat -an | find ":8080"
-FOR /F %%a in ('netstat -an ^| find "127.0.0.1:8080"') do set /A count = count + 1
+netstat -an | find "0.0.0.0:8080"
+FOR /F %%a in ('netstat -an ^| find "0.0.0.0:8080"') do set /A count = count + 1
 
 REM If it is, open the browser. Otherwise, try again
 if %count% GEQ 1 (
